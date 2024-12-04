d29a5d266b6991d6571f9c7316d632104636a39d
WallasAR <rochaaguiar2016@gmail.com>
2024-12-03 21:30:20 -0300

    iframes-optimization

--- js/index.js
+++ js/index.js
@@ -114,6 +114,21 @@ function initializeLazyLoading(images) {
   images.forEach((img) => observer.observe(img));
 }
 
+document.querySelectorAll(".video-container").forEach((container) => {
+  const videoId = container.dataset.videoId;
+  const iframe = document.createElement("iframe");
+  iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+  iframe.allow =
+    "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+  iframe.allowFullscreen = true;
+
+  // Evento de clique para carregar o iframe
+  container.addEventListener("click", () => {
+    container.classList.add("active");
+    container.appendChild(iframe);
+  });
+});
+
 
 document.addEventListener("DOMContentLoaded", function () {
   const accordionItems = document.querySelectorAll("#accordionFlush .accordion-item");
